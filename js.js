0b81dccd47338b981789ef1c9e7a016758ede950
matt-howells <matthowells@icloud.com>
2024-10-23 11:46:27 -0700

    create add, subtract, muultiply & divide func

---
+++ js.js
@@ -0,0 +1,16 @@
+const add = function(a,b) {
+    return a + b
+}
+
+const subtract = function(a, b) {
+    return a - b
+}
+
+const multiply = function(array) {
+    return array.reduce((product, current) => product * current, 1)
+}
+
+const divide = function(a, b) {
+    return a / b
+}
+
