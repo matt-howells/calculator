0a9f5b94fbe5652ee5c2c9b33da4df4aef93ae69
matt-howells <matthowells@icloud.com>
2024-10-23 12:25:11 -0700

    create operate function

--- js.js
+++ js.js
@@ -1,3 +1,25 @@
+// operate function
+
+let a = 0
+let b = 0
+let operator = ''
+
+const operate = function(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    } else {
+        return "Error"
+    }
+}
+
+//math functions
+
 const add = function(a,b) {
     return a + b
 }
