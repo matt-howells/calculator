566ad7301cd9a17ef8a55087ce037e8c8370b75d
matt-howells <matthowells@icloud.com>
2024-10-24 12:46:15 -0700

    implement operate function

--- js.js
+++ js.js
@@ -1,23 +1,3 @@
-//general operate function
-
-let a = 0
-let b = 0
-let operator = ''
-
-const operate = function(a, b, operator) {
-    if (operator === '+') {
-        return add(a, b);
-    } else if (operator === '-') {
-        return subtract(a, b);
-    } else if (operator === '*') {
-        return multiply(a, b);
-    } else if (operator === '/') {
-        return divide(a, b);
-    } else {
-        return "Error"
-    }
-}
-
 //basic math functions
 
 const add = function(a,b) {
@@ -34,10 +14,25 @@ const multiply = function(a, b) {
 
 const divide = function(a, b) {
     if (b === 0) {
-        return "Error: Division by zero!";
+        return "no stop";
     }
     return a / b;
 }
+
+const operate = function(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    } else {
+        return "Error"
+    }
+}
+
 
 // populate display 
 
@@ -57,21 +52,45 @@ const updateDisplay = function(value) {
     display.textContent = currentValue;
 }
 
+let a = '';
+let b = '';
+let operator = '';
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
         
         if (button.classList.contains('numBtn')) {
             updateDisplay(buttonText);
+
         } else if (buttonText === 'AC') {
-            currentValue = '0';
-            display.textContent = currentValue;
-        } else if (buttonText === '=') {
             
-        } else {
+            currentValue = '0';
+            a = '';
+            b = '';
+            display.textContent = currentValue;
+
+        } else if (['+', '-', '*', '/'].includes(buttonText)) {
+           
+            a = parseFloat(currentValue);
+            operator = buttonText;
+            currentValue = '0';
+
+        } else if (buttonText === '=') {
+           
+            b = parseFloat(currentValue);
+            const result = operate(a, b, operator);
+            currentValue = result.toString();
+            display.textContent = currentValue;
+
+            a = '';
+            b = '';
+            operator = '';
 
         }
     });
 })
+
+
 
 
